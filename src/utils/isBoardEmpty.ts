3726a2c148e4130cffc65067ba1cfe8107d09fd3
selly361 <104414846+selly361@users.noreply.github.com>
2023-01-27 11:41:57 +0000

    removed default code in app.tsx and index.tsx

--- src/utils/isBoardEmpty.ts
+++ src/utils/isBoardEmpty.ts
@@ -1,5 +1,6 @@
-import { Tboard } from "types/types" 
-export function isBoardEmpty(board){
+import { Tboard } from "types/types"
+
+export function isBoardEmpty(board: Tboard){
   const isEmpty = board.some(square => typeof square == "number")
 
   return isEmpty;
